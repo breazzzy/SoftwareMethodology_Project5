07a1ab5c7c0f63f083d9539df886334b02e53704
AHN333 <100101126+AHN333@users.noreply.github.com>
2023-04-26 19:42:09 -0400

    Donut class

--- app/src/main/java/com.group9.project5/model/DonutAdapter.java
+++ app/src/main/java/com.group9.project5/model/DonutAdapter.java
@@ -1,6 +1,9 @@
 package com.group9.project5.model;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,12 +11,14 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.group9.project5.R;
+import com.group9.project5.activities.DonutSelectedActivity;
 
 import java.util.ArrayList;
 
@@ -56,17 +61,17 @@ public class DonutAdapter extends RecyclerView.Adapter<DonutAdapter.DonutHolder>
             donutView = (TextView) itemView.findViewById(R.id.donutFlavor);
             priceView = (TextView) itemView.findViewById(R.id.donutPrice);
             imageView = (ImageView) itemView.findViewById(R.id.donutImage);
-            /*btn_add = itemView.findViewById(R.id.btn_add);
+            btn_add = itemView.findViewById(R.id.btn_add);
             setAddButtonOnClick(itemView); //register the onClicklistener for the button on each row.
 
             /* set onClickListener for the row layout,
-             clicking on a row will navigate to another Activity
+             clicking on a row will navigate to another Activity*/
 
-            parentLayout.setOnClickListener(new View.OnClickListener() {
+            /*parentLayout.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Intent intent = new Intent(itemView.getContext(), DonutSelectedActivity.class);
-                    intent.putExtra("DONUT", donutname.getText());
+                    intent.putExtra("DONUT", donutView.getText());
                     itemView.getContext().startActivity(intent);
                 }
             });*/
@@ -77,31 +82,31 @@ public class DonutAdapter extends RecyclerView.Adapter<DonutAdapter.DonutHolder>
          * Clicking on the button will create an AlertDialog with the options of YES/NO.
          * @param itemView
          */
-      /*  private void setAddButtonOnClick(@NonNull View itemView) {
+        private void setAddButtonOnClick(@NonNull View itemView) {
             btn_add.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     AlertDialog.Builder alert = new AlertDialog.Builder(itemView.getContext());
                     alert.setTitle("Add to order");
-                    alert.setMessage(donutname.getText().toString());
+                    alert.setMessage(donutView.getText().toString());
                     //handle the "YES" click
                     alert.setPositiveButton("yes", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
                             Toast.makeText(itemView.getContext(),
-                                    donutname.getText().toString() + " added.", Toast.LENGTH_LONG).show();
+                                    donutView.getText().toString() + " added.", Toast.LENGTH_LONG).show();
                         }
                         //handle the "NO" click
                     }).setNegativeButton("no", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
                             Toast.makeText(itemView.getContext(),
-                                    donutname.getText().toString() + " not added.", Toast.LENGTH_LONG).show();
+                                    donutView.getText().toString() + " not added.", Toast.LENGTH_LONG).show();
                         }
                     });
                     AlertDialog dialog = alert.create();
                     dialog.show();
                 }
             });
-        }*/
+        }
 
     }
 
