0cb4010b6ad64e15b4d5ef4996c4dad63fe5c602
AHN333 <100101126+AHN333@users.noreply.github.com>
2023-04-26 17:07:08 -0400

    Donut class

--- app/src/main/java/com.group9.project5/activities/DonutActivity.java
+++ app/src/main/java/com.group9.project5/activities/DonutActivity.java
@@ -28,12 +28,18 @@ public class DonutActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.donutRCV);
 
         ArrayList<Donut> donuts = new ArrayList<Donut>();
-        donuts.add(new Donut("strawberry", R.drawable.strawberryfrosting, "1.79"));
-        donuts.add(new Donut("strawberry", R.drawable.strawberryfrosting, "1.79"));
-        donuts.add(new Donut("strawberry", R.drawable.strawberryfrosting, "1.79"));
-        donuts.add(new Donut("strawberry", R.drawable.strawberryfrosting, "1.79"));
-        donuts.add(new Donut("strawberry", R.drawable.strawberryfrosting, "1.79"));
-        donuts.add(new Donut("strawberry", R.drawable.strawberryfrosting, "1.79"));
+        donuts.add(new Donut("Plain", R.drawable.plain, "1.79"));
+        donuts.add(new Donut("Blueberry", R.drawable.blueberry, "1.79"));
+        donuts.add(new Donut("Boston Creme", R.drawable.boston, "1.79"));
+        donuts.add(new Donut("Chocolate", R.drawable.chocolate, "1.79"));
+        donuts.add(new Donut("Chocolate Icing", R.drawable.chocolatefrosting, "1.79"));
+        donuts.add(new Donut("Vanilla Icing", R.drawable.vanillafrosting, "1.79"));
+        donuts.add(new Donut("Strawberry Icing", R.drawable.strawberryfrosting, "1.79"));
+        donuts.add(new Donut("Glazed", R.drawable.glazed, "1.79"));
+        donuts.add(new Donut("Cruller", R.drawable.cruller, "1.79"));
+        donuts.add(new Donut("Powdered", R.drawable.powedered, "1.79"));
+        donuts.add(new Donut("Jelly", R.drawable.jelly, "1.79"));
+        donuts.add(new Donut("Lemon", R.drawable.lemon, "1.79"));
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new DonutAdapter(getApplicationContext(), donuts));
